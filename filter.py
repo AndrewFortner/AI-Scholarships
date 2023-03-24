0ce5e3b3e1cef5836271174164430336ffdf9edd
AndrewFortner <fortnerand@gmail.com>
2023-03-23 22:03:28 -0500

    reorganized the filtered csv

--- filter.py
+++ filter.py
@@ -8,5 +8,12 @@ with open('scholarship-info.csv', 'r') as f:
         for row in reader:
             for item in row:
                 if 'http://' in item:
+                    split = item.split(' ')
+                    link = ''
+                    for i in range(len(split)):
+                        if 'http://' in split[i]:
+                            link = split[i]
+                            break
+                    row.insert(0, "Link: {}".format(link))
                     writer.writerow(row)
                     break
